c8e62ffa9070b48be083b2be97d1b006699f9d63
lxgxrto <amitolxgxrto@gmail.com>
2015-09-14 00:50:56 -0500

    Agregando los caminos posibles para el analisis sintactico

--- Compilador2/src/compilador2/Sintactico.java
+++ Compilador2/src/compilador2/Sintactico.java
@@ -12,31 +12,51 @@ import java.util.ArrayList;
  * @author Tsukunowo
  */
 public class Sintactico {
+
     ArrayList comparacion;
     ArrayList<Token> comparacion2;
-    public Sintactico(ArrayList aux){
-        comparacion=aux;
+
+    public Sintactico(ArrayList aux) {
+        comparacion = aux;
     }
-    public boolean Buscar(int posicion, ArrayList<Token> sentencia)
-    {
-            if(sentencia.get(posicion).codigo==54)
-            {
-                Buscar(posicion+1,sentencia);
-            }
-            if(sentencia.get(posicion).codigo==90 || sentencia.get(posicion).codigo==302)
-                return true;
+
+    public boolean BuscarMain(int posicion, ArrayList<Token> sentencia) {
+        if (sentencia.get(posicion).codigo == 54) {
+            BuscarMain(posicion + 1, sentencia);
+        }
+        if (sentencia.get(posicion).codigo == 1) {
+            BuscarMain(posicion + 1, sentencia);
+        }
+        if (sentencia.get(posicion).codigo == 90 || sentencia.get(posicion).codigo == 302) {
+            return true;
+        }
         return false;
     }
-    public void EncontrarMain(){
-        for (Object i : comparacion) {
-            ArrayList<Token> aux=(ArrayList<Token>) i;
-            for (int j=0; j<aux.size();j++)
-            {
-                Buscar(j,aux);
-            }
-                
-        }
+
+    public boolean BuscarAsignacion(int posicion, ArrayList<Token> sentencia) {
+        if (sentencia.get(posicion).codigo >= 1000)
+            BuscarAsignacion(posicion + 1, sentencia);
+        if (sentencia.get(posicion).codigo == 250){
+            BuscarOperacion(posicion + 1, sentencia);
+        }    
+        return false;
+    }
+
+    public boolean BuscarOperacion(int posicion, ArrayList<Token> sentencia){
         
+        return false;
     }
     
+    public void Analizar() {
+        for (Object i : comparacion) {
+            ArrayList<Token> aux = (ArrayList<Token>) i;
+            for (int j = 0; j < aux.size(); j++) {
+                BuscarMain(j, aux);
+
+            }
+
+        }
+
+    }
+
 }
