fc5ce0465a4303084e4b78e7c7fc6ce59508f3db
Tsukunowo <Tsukunowo@DESKTOP-V94LB6R>
2015-09-14 01:20:16 -0500

    if, for, op, while

--- Compilador2/src/compilador2/Sintactico.java
+++ Compilador2/src/compilador2/Sintactico.java
@@ -41,9 +41,50 @@ public class Sintactico {
         }    
         return false;
     }
+    
+    public boolean BuscarFor(int posicion, ArrayList<Token> sentencia)
+    {
+        if (sentencia.get(posicion).codigo == 62)
+            BuscarFor(posicion + 1, sentencia);
+        if (sentencia.get(posicion).codigo == 450)
+            BuscarAsignacion(posicion + 1, sentencia);
+        return false;
+    }
+    
+    public boolean BuscarWhile(int posicion, ArrayList<Token> sentencia)
+    {
+        if (sentencia.get(posicion).codigo == 82)
+            BuscarFor(posicion + 1, sentencia);
+        if (sentencia.get(posicion).codigo == 450)
+            BuscarAsignacion(posicion + 1, sentencia);
+        return false;
+    }
+    
+    public boolean BuscarIf(int posicion, ArrayList<Token> sentencia)
+    {
+        if (sentencia.get(posicion).codigo == 78)
+            BuscarFor(posicion + 1, sentencia);
+        if (sentencia.get(posicion).codigo == 450)
+            BuscarAsignacion(posicion + 1, sentencia);
+        return false;
+    }
 
-    public boolean BuscarOperacion(int posicion, ArrayList<Token> sentencia){
-        
+    public boolean BuscarOperacion(int posicion, ArrayList<Token> sentencia)
+    {
+        if (sentencia.get(posicion).codigo >= 1000)
+            BuscarOperacion(posicion + 1, sentencia);
+        if (sentencia.get(posicion).codigo == 200)
+            BuscarOperacion(posicion + 1, sentencia);
+        if (sentencia.get(posicion).codigo == 201)
+            BuscarOperacion(posicion + 1, sentencia);
+        if (sentencia.get(posicion).codigo == 202)
+            BuscarOperacion(posicion + 1, sentencia);
+        if (sentencia.get(posicion).codigo == 203)
+            BuscarOperacion(posicion + 1, sentencia);
+        if (sentencia.get(posicion).codigo == 204)
+            BuscarOperacion(posicion + 1, sentencia);
+        if (sentencia.get(posicion).codigo == 302)
+            return true;
         return false;
     }
     
